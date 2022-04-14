754fb2dd3748935ae2051692ac77261c6ec58a6f
kawagh <kawagh@example.com>
2022-04-14 10:03:49 +0900

    show recent commits in not github repositories

--- ghst.go
+++ ghst.go
@@ -23,25 +23,33 @@ func main() {
 		log.Fatal(err)
 	}
 	grps := strings.Split(string(ghq_root_paths), "\n")
-	for _, grp := range grps {
-		fmt.Println(grp)
-		if strings.HasSuffix(grp, "ghq") {
+	for _, ghq_root_path := range grps {
+		if ghq_root_path == "" {
+			continue
+		}
+		if strings.HasSuffix(ghq_root_path, "ghq") {
 			for _, git_user_name := range git_user_names {
-				github_repos_with_user := grp + "/github.com/" + git_user_name
-				repositories, err := ioutil.ReadDir(github_repos_with_user)
-				if err != nil {
-					log.Fatal(err)
-				}
-				for _, repo := range repositories {
-					if repo.IsDir() {
-						git_repo_path := github_repos_with_user + "/" + repo.Name()
-						log, err := get_recent_git_logs(git_repo_path)
-						if err == nil && log != "" {
-							fmt.Println(git_repo_path)
-							fmt.Println(log)
-						}
-					}
-				}
+				github_repos_roots_with_user := ghq_root_path + "/github.com/" + git_user_name
+				print_recent_commits_each_repos(github_repos_roots_with_user)
+			}
+		} else {
+			print_recent_commits_each_repos(ghq_root_path)
+		}
+	}
+}
+
+func print_recent_commits_each_repos(git_repo_path string) {
+	repositories, err := ioutil.ReadDir(git_repo_path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, repo := range repositories {
+		if repo.IsDir() {
+			repo_path := git_repo_path + "/" + repo.Name()
+			log, err := get_recent_git_logs(repo_path)
+			if err == nil && log != "" {
+				fmt.Println(repo.Name())
+				fmt.Println(log)
 			}
 		}
 	}
